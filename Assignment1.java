e8b4856e52f4f3c0c69dba3806262fc668d63c2e
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2025-08-13 22:19:33 +0530

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -15,9 +15,9 @@ public class Assignment1 {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 		driver.get("https://amazon.in");
 		driver.findElement(By.id("nav-link-accountList-nav-line-1")).click();
-		driver.findElement(By.id("ap_email_login")).sendKeys("9980865436");
+		driver.findElement(By.id("ap_email_login")).sendKeys("Phone_No");
 		driver.findElement(By.xpath("//input[@type='submit']")).click();
-		driver.findElement(By.id("ap_password")).sendKeys("Jaishreeram@123");
+		driver.findElement(By.id("ap_password")).sendKeys("Your_Password");
 		driver.findElement(By.id("signInSubmit")).click();
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Laptop");
 		driver.findElement(By.id("nav-search-submit-button")).click();
