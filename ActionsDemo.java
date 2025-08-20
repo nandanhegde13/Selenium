d8921d5dc31d94d633331af0d799b8948287f250
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2025-08-20 10:39:34 +0530

    Add files via upload

--- ActionsDemo.java
+++ ActionsDemo.java
@@ -1,22 +1,20 @@
-package Test;
+package Default;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
 public class ActionsDemo {
 
-	public static void main(String[] args) {
-		
+	public static void main(String args[]) {
+       
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.flipkart.com/");
-        Actions a = new Actions(driver);
-        a.moveToElement(driver.findElement(By.xpath("//a[@title='Login']"))).build().perform();
-        a.moveToElement(driver.findElement(By.className("Pke_EE"))).click().keyDown(Keys.SHIFT).sendKeys("HELLO").doubleClick().contextClick().build().perform();
-        
+		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+		Actions a = new Actions(driver);
+		a.moveToElement(driver.findElement(By.id("username"))).click().keyDown(Keys.SHIFT).sendKeys("Hello").doubleClick().contextClick().build().perform();
+		
+	
 	}
-
 }
