04e3902f68abdbff796957cbb92d302426935da4
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2025-08-30 00:39:58 +0530

    Add files via upload

---
+++ ExceptionDemo.java
@@ -0,0 +1,23 @@
+package Default;
+
+public class ExceptionDemo {
+	
+	
+	public static void main(String args[])
+	{
+		
+		int a=10;
+		int b=0;
+		
+		try {
+			System.out.println(a/b);
+			
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+		System.out.println("Nandan");
+	}
+
+}
