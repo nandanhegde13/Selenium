16ba3706c542215fc22428ff72add4bb3cf6e670
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2025-09-02 14:51:03 +0530

    Add files via upload

---
+++ ParentAirCraft.java
@@ -0,0 +1,17 @@
+package Default;
+
+public abstract class ParentAirCraft {
+	
+	public void engine()
+	{
+		System.out.println("Follow engine guidlines");
+	}
+	
+	public void safetyGuidlines()
+	{
+		System.out.println("Follow safety guidlines");
+	}
+
+	
+	public abstract void bodyColor();
+}
